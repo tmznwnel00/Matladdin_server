a14661fc1d67fc3f65f19998f467fd8a1b48ca7e
tmznwnel00 <tmznwnel00@gmail.com>
2023-12-17 11:34:14 +0900

    fix: https

--- app.py
+++ app.py
@@ -268,4 +268,4 @@ def get_restuarant():
 
 if __name__ == '__main__':
     ssl_context = ('cert.pem', 'key.pem')
-    app.run(ssl_context=ssl_context, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000, ssl_context=ssl_context)
